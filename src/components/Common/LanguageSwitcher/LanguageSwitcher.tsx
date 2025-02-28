c920066bf393032a5b1c6c942fa057129294fc47
Từ Thái Phong <tuthaiphong701@gmail.com>
2025-02-28 17:00:38 +0700

    fix build error

--- src/components/Common/LanguageSwitcher/LanguageSwitcher.tsx
+++ src/components/Common/LanguageSwitcher/LanguageSwitcher.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 // import { useTranslation } from "react-i18next";
-import i18n from "../../../i18n";
+import i18n from "../../../../i18n";
 
 const LanguageSwitcher = () => {
   //   const { i18n } = useTranslation();
